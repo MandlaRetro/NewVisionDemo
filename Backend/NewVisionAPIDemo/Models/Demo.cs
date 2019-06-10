5cc50f50609b28826d55007e69c6c65b75409ea3
Mandla <mandlamasombuka21@gmail.com>
2019-06-10 11:05:17 +0200

    backend with postgres database

---
+++ Backend/NewVisionAPIDemo/Models/Demo.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NewVisionAPIDemo.Models {
+    public class Demo {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+        public string ImageBase64 { get; set; }
+    }
+}
---
+++ Backend/NewVisionAPIDemo/Models/ModelsContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NewVisionAPIDemo.Models {
+    public class ModelsContext : DbContext{
+        public ModelsContext(DbContextOptions<ModelsContext> options) : base(options) { }
+
+        public DbSet<Demo> Demos { get; set; }
+    }
+}
